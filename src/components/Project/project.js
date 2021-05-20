9cca01217ba5889f13be7a0e01001d83d007afe6
Aubrey Bennett <27aubreyb@gmail.com>
2021-05-19 20:38:08 -0400

    added container to project component for styling

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -4,6 +4,7 @@ import React from 'react';
 function Project({ img, title, description, githublink, applink }) {
     return (
         <>
+        <div className="container">
        <div className="row">
                 <div className="col-lg-12">
                     <div className="card" style={{ width: "100%" }}>
@@ -18,6 +19,7 @@ function Project({ img, title, description, githublink, applink }) {
                         </div>
                     </div>
                 </div>
+            </div>
             </div>
         </>
     )
