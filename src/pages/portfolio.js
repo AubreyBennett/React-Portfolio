9767ba7f1e8395257f7523b1492e9525cf07261e
Aubrey Bennett <27aubreyb@gmail.com>
2021-05-19 20:34:39 -0400

    fixed images and added project components for portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,53 +1,40 @@
 import React from 'react';
+import Project from '../components/Project/project';
+
+import passAuxImg from '../Assets/passme.jpg';
+import passwordImg from '../Assets/password.jpg';
+import workdayImg from '../Assets/workday.jpg';
+
 
 function Portfolio() {
     return (
         <div>
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="card" style={{ width: "100%" }}>
-                        <img src="../Assets/passme.jpg" className="card-img-top" alt="Responsive" />
-                        <div className="card-b">
-                            <h5 className="card-title" style= {{textAlign: "center" }}>Pass Me The Aux</h5>
-                            <p className="card-text" style= {{textAlign: "center" }}>Music App that allows you to lookup an artist and a song by
-                            that artist and then provides you with the lyrics to that song and similar songs produced by the artist.
-                            </p>
-                            <a href="https://github.com/jpecheverryp/pass-me-the-aux" className="card-link">Github link</a>
-                            <a href="https://jpecheverryp.github.io/pass-me-the-aux/" className="card-link">Application link</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Project
+                title = "Pass Me The Aux"
+                img = {passAuxImg}
+                description = {`Music App that allows you to lookup an artist and a song by
+                that artist and then provides you with the lyrics to that song and similar songs produced by the artist.`}
+                githublink="https://github.com/jpecheverryp/pass-me-the-aux" 
+                applink = "https://jpecheverryp.github.io/pass-me-the-aux/"
+             />
+            
+            <Project 
+                title = "Workday Scheduler"
+                img = {workdayImg}
+                description = {`Daily planner that will help you manage your time
+                effectively.`}
+                githublink="https://github.com/AubreyBennett/Workday-scheduler"  
+                applink = "https://aubreybennett.github.io/Workday-scheduler/" 
+            />
 
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="card" style={{ width: "100%" }}>
-                        <img src="../Assets/workday.jpg" className="card-img-top" alt="Responsive" />
-                        <div className="card-b">
-                            <h5 className="card-title" style= {{textAlign: "center" }}>Workday Scheduler</h5>
-                            <p className="card-text" style= {{textAlign: "center" }}>Daily planner that will help you manage your time
-                            effectively.</p>
-                            <a href="https://github.com/AubreyBennett/Workday-scheduler" className="card-link">Github link</a>
-                            <a href="https://aubreybennett.github.io/Workday-scheduler/" className="card-link">Application link</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="card" style={{ width: "100%" }}>
-                        <img src="../Assets/password.jpg" className="card-img-top" alt="Responsive" />
-                        <div className="card-b">
-                            <h5 className="card-title" style= {{textAlign: "center" }}>Password Generator</h5>
-                            <p className="card-text" style= {{textAlign: "center" }}>Generates random passwords with the characters you
-                            desire.</p>
-                            <a href="https://github.com/AubreyBennett/Password-Generator" className="card-link" >Github link</a>
-                            <a href="https://aubreybennett.github.io/Password-Generator/" className="card-link">Application link</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Project
+                title = "Password Generator"
+                img = {passwordImg}
+                description = {`Generates random passwords with the characters you
+                desire.`}
+                githublink="https://github.com/AubreyBennett/Password-Generator"  
+                applink = "https://aubreybennett.github.io/Password-Generator/" 
+            />
         </div>
     );
 }
